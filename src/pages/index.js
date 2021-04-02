4d6a443a73fd3df17040fe34c405da6ac0d925f2
nison79 <georgenison@gmail.com>
2021-04-02 13:14:19 +0300

    Button

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,12 @@ const ContentPage = styled.div`
   display:grid;
   grid-template-columns:1fr 2fr 1fr ;
   grid-template-rows:1fr 2fr 1fr;
-  @media(max-width:600px) {
+  @media(max-width:800px) {
     grid-template-rows:1fr 300px 1fr;
   }
   
   p{
-    font-size:2rem;
+    font-size:1rem;
     grid-column-start:2;
     grid-column-end:3;
     grid-row-start:3;
@@ -26,30 +26,61 @@ const ContentPage = styled.div`
     z-index:3;
     font-family:'Avenir' ,sans-serif;
     color:white;
-    font-size:3rem;
-    @media(max-width:978px) {
-    font-size:2rem;
-    }
-    @media(max-width:650px) {
-    font-size:1.2rem;
-    }
-    @media(min-width:1500px) {
-    font-size:3rem;
+
+
+    @media(min-width:499px) {
+    font-size:1rem;
     grid-row-start:2;
     grid-row-end:3; 
     align-self:end;
-  }
-  @media(min-width:1970px) {
-    font-size:3rem;
+    margin-bottom:2rem;
+    }
+    @media(min-width:658px) {
+    font-size:1.2rem;
+    align-self:end;
+    margin-top:10rem;
     grid-row-start:2;
     grid-row-end:3; 
+    }
+    @media(min-width:800px) {
+    font-size:2rem;
+    align-self:end;
+    margin-top:10rem;
+    grid-row-start:2;
+    grid-row-end:3; 
+    }
+    @media(min-width:978px) {
+    font-size:2.5rem;
+    grid-row-start:2;
+    grid-row-end:3; 
+    align-self:end;
+    margin-bottom:3rem;
+    }
+    @media(min-width:1200px) {
+    font-size:2.9rem;
+    grid-row-start:2;
+    grid-row-end:3; 
+    align-self:end;
+    margin-bottom:rem;
+  }
+    @media(min-width:1500px) {
+    font-size:2.5rem;
+    grid-row-start:2;
+    grid-row-end:3; 
+    align-self:end;
     margin-bottom:10rem;
   }
-  @media(min-width:2180px) {
-    font-size:3rem;
+  @media(min-width:1970px) {
+    font-size:4rem;
     grid-row-start:2;
     grid-row-end:3; 
-    margin-bottom:20rem;
+    margin-bottom:30rem;
+  }
+  @media(min-width:2180px) {
+    font-size:4rem;
+    grid-row-start:2;
+    grid-row-end:3; 
+    margin-bottom:40rem;
   }
   }
 `
@@ -92,24 +123,28 @@ const Title = styled.h1`
   display:flex;
   flex-direction:column;
 
-
-  span{
-    color:violet;
+  @media(max-width:1250px) {
+    font-size:3.5rem;
   }
+  
   @media(max-width:1102px) {
-    font-size:4rem;
+    font-size:3.5rem;
   }
   @media(max-width:888px) {
-    font-size:3.7rem;
+    font-size:2.6rem;
   }
   @media(max-width:810px) {
-    font-size:3rem;
+    font-size:2em;
   }
   @media(max-width:675px) {
-    font-size:2rem;
+    font-size:1.8rem;
   }
   @media(max-width:454px) {
-    font-size:1.8rem;
+    font-size:1.1rem;
+  }
+
+  span{
+    color:#85B6FE;
   }
 `
 
@@ -118,10 +153,75 @@ const  StyledButton = styled.button`
   grid-column-end: 4;
   grid-row-start: 2;
   grid-row-end: 3;
-  width:100px;
-  height:50px;
+  width:180px;
+  height:60px;
   align-self:center;
   justify-self:center;
+  background-color:#85B6FE;
+  font-family:'Avenir' ,sans-serif;
+  font-size:0.8rem;
+  color:white;
+  border:none;
+  border-radius:5px;
+  cursor:pointer;
+  @media(min-width:500px) {
+    width:80px;
+    height:40px;
+    font-size:0.9rem;
+  }
+  @media(min-width:800px) {
+    width:150px;
+    height:60px;
+    margin-bottom:8rem;
+    font-size:1.5rem;
+  }
+  @media(min-width:900px) {
+    width:180px;
+    height:60px;
+    margin-bottom:15rem;
+    font-size:1.5rem;
+  }
+  @media(min-width:1400px) {
+    width:250px;
+    height:70px;
+    margin-bottom:25rem;
+    font-size:1.5rem;
+  }
+  @media(min-width:1500px) {
+    width:250px;
+    height:70px;
+    margin-bottom:30rem;
+    font-size:1.5rem;
+  }
+  @media(min-width:1700px) {
+    width:250px;
+    height:70px;
+    margin-bottom:35rem;
+    font-size:1.5rem;
+  }
+  @media(min-width:1900px) {
+    width:250px;
+    height:70px;
+    margin-bottom:48rem;
+    font-size:1.5rem;
+  }
+  @media(min-width:2000px) {
+    width:250px;
+    height:70px;
+    margin-bottom:57rem;
+    font-size:1.5rem;
+  }
+  @media(min-width:2000px) {
+    width:250px;
+    height:70px;
+    margin-bottom:65rem;
+    font-size:1.5rem;
+  }
+`
+
+const StyledLink = styled(Link)`
+  color:white;
+  width:inherit;
 `
 
 const IndexPage = () => (
@@ -129,10 +229,10 @@ const IndexPage = () => (
     <ContentPage>
         <SEO title="Home" />
         <Menu>
-          <h2>marbles-greece</h2>
+          <StyledLink to="/"><h2>marbles-greece</h2></StyledLink>
           <h2>about</h2>
           <h2>contact</h2>
-          <h2>companies</h2>
+          <StyledLink to="/producers"><h2>producers</h2></StyledLink>
         </Menu>
         <StaticImage
             src= '../images/bg3.jpg'
@@ -147,7 +247,9 @@ const IndexPage = () => (
         <Title>The Material that Highlights<br></br>The <span>Beauty,</span>For Centuries.
         </Title>
         <p>from the best producers and experts in the world.</p>
-        <StyledButton>Find Out</StyledButton>
+        <StyledButton>
+          <StyledLink to="/producers">Find Out</StyledLink>
+        </StyledButton>
       </ContentPage>
     
 
---
+++ src/pages/producers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const producers = () => {
+  return (
+    <div>
+      <h1>Producers</h1>
+    </div>
+  )
+}
+
+export default producers
